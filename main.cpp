75ade5ec1c4d05a12126289a533009e87413b643
Jameson York <jamesonyork8@gmail.com>
2020-05-25 20:20:04 +1000

    To Match.

--- main.cpp
+++ main.cpp
@@ -4,27 +4,22 @@
 #include <Curie/RM.h>
 #include <Curie/SB.h>
 
-#include <SDL2/SDL.h>
-
 #include <cstdio>
 
 int32_t main(int32_t argc, char* args[])
 {
-    VL::system s;
+    VL::System s;
 
-    VL::window w;
+    VL::Window w;
 
     {
         Quartz q(25);
+        RM rm(q, *w.window);
+        SB sb(q, 1);
 
         {
-            RM rm(q, *w.window_handle);
-            SB sb(q, 1);
-
-            {
-                Game g(q, rm, sb);
-                g.run();
-            }
+            Game g(q, rm, sb);
+            g.run();
         }
     }
 
