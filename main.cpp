041de3037cfc083782866b8eae1286ae9d208b35
Jameson York <jamesonyork8@gmail.com>
2020-07-01 21:20:36 +1000

    New build.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <Game.h>
 
+#include <Curie/IL.h>
 #include <Curie/Quartz.h>
 #include <Curie/RM.h>
 #include <Curie/SB.h>
+#include <Curie/VL.h>
 
 #include <cstdio>
 
 int32_t main(int32_t argc, char* args[])
 {
-    Curie::VL::System s;
+    Curie::IL::System input_sys;
 
+    Curie::VL::System video_sys;
     Curie::VL::Window w;
 
     {
